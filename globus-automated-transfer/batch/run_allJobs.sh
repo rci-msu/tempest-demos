66f145e2771c1066460bc17345c72705d706da5e
Trevor Vannoy <trevorvannoy@montana.edu>
2025-04-09 20:09:19 +0000

    remove extra space

--- globus-automated-transfer/batch/run_allJobs.sh
+++ globus-automated-transfer/batch/run_allJobs.sh
@@ -2,7 +2,7 @@
 transferJob=$(sbatch run_transferData.sbatch | cut -d ' ' -f 4)
 
 # Run the data processing script after the data transfer job is completed.
-processJob=$(sbatch  --dependency=afterok:"$transferJob" run_processData.sbatch | cut -d ' ' -f 4)
+processJob=$(sbatch --dependency=afterok:"$transferJob" run_processData.sbatch | cut -d ' ' -f 4)
 
 # Run the cleanup job after the data processing job is complete.
 sbatch --dependency=afterok:"$processJob" run_cleanupData.sbatch
